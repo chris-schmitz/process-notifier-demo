bfdade8634b8881d345f2ee46bcdde070270cff5
Christopher Schmitz <cschmitz@labelinsight.com>
2021-11-05 15:49:15 -0500

    Removes setters from DTOs now that we have factories

--- src/main/java/com/schmitz/processnotifierdemo/dto/Message.java
+++ src/main/java/com/schmitz/processnotifierdemo/dto/Message.java
@@ -8,16 +8,9 @@ public class Message {
         return content;
     }
 
-    public void setContent(String content) {
-        this.content = content;
-    }
 
     public String getTo() {
         return to;
-    }
-
-    public void setTo(String to) {
-        this.to = to;
     }
 
 }
